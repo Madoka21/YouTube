9b19b36266c150097c6fa5c4ac052b5e9ce3554e
Madoka21 <108270199+Madoka21@users.noreply.github.com>
2022-06-27 16:49:25 +0600

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,10 @@ from pytube import YouTube
 
 # py -m pip install pytube
 
-token = '5466706932:AAF_Ua9V0AAPUNuZ2rRyxIHZqY9XdeEc2bs'
+server = flask.Flask(__name__)
+
+app_name = "youtubebot1234"
+token = '5315904167:AAGRl-txzHs9lKvWcZ4en-57ld3heO82O-o'
 bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands=['start'])
@@ -24,7 +27,21 @@ def get_video(msg):
     bot.send_video(msg.chat.id, file)
     bot.send_message(msg.chat.id, 'Приятного просмотра')
 
-bot.polling()
+@server.route('/' + token, methods=['POST'])
 
+def get_message():
+      bot.process_new_updates([types.Update.de_json(flask.request.stream.read().decode("utf-8"))])
+      return "!", 200
+
+@server.route('/', methods=["GET"])
+
+def index():
+    print("hello webhook!")
+    bot.remove_webhook()
+    bot.set_webhook(url=f"https://{app_name}.herokuapp.com/{token}")
+    return "Hello from Heroku!", 200
+
+if __name__ == "__name__"":
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
 
 # py -m pip install pytelegrambotapi
